7c2f60c2543b9575eef93314757b25fc7fdd3606
Anamika Kashyap <anamika.kashyap@trayistats.com>
2025-07-23 12:37:08 +0530

    changed project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,22 @@
-// components/Projects.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Projects.css";
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaGithub } from "react-icons/fa";
+
+function getTechIcon(tech) {
+  switch (tech.toLowerCase()) {
+    case "html":
+      return <FaHtml5 color="#e34c26" title="HTML5" />;
+    case "css":
+      return <FaCss3Alt color="#2965f1" title="CSS3" />;
+    case "javascript":
+      return <FaJsSquare color="#f0db4f" title="JavaScript" />;
+    case "github pages":
+      return <FaGithub color="#fff" title="GitHub Pages" />;
+    default:
+      return <span>{tech}</span>;
+  }
+}
 
 function Projects() {
   const [projects, setProjects] = useState([]);
@@ -24,12 +39,23 @@ function Projects() {
           <div className="project-card-portfolio" key={project.id}>
             <h3>{project.name}</h3>
             <p>{project.description}</p>
+
+            {/* Tech Stack with Icons */}
+            <div className="tech-stack-icons">
+              {project.techStack?.map((tech, index) => (
+                <div className="tech-icon" key={index}>
+                  {getTechIcon(tech)}
+                  <span>{tech}</span>
+                </div>
+              ))}
+            </div>
+
             <div className="button-project">
               <a href={project.codeLink} target="_blank" rel="noreferrer">
-                View Code
+                🚀 View Code
               </a>
               <a href={project.liveLink} target="_blank" rel="noreferrer">
-                View Live Link
+                🌐 Live Demo
               </a>
             </div>
           </div>
